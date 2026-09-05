666cf35b2d8f35967b57c375804302b0c7153a52
Bakr <mohamed.bakr45686@gmail.com>
2026-09-05 18:46:13 +0300

    login function

---
+++ Backend/src/controllers/auth.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
+import User from '../models/user.model';
+
+const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    if(!email || !password) {
+      return res.status(400).json({ message: 'Email and password are required' });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    res.status(200).json({ message: 'Login successful', user });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
+}
