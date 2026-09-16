babb4ecb2a9db98e25ea13d9ef92fd7e83d71ccf
Youssef ehab shehata abd elhaseeb <yousefeha026@gmail.com>
2026-09-16 18:51:26 +0300

    Delete Backend directory

--- Backend/src/models/types/adjacencyItemInterface.ts
+++
@@ -1,6 +0,0 @@
-import { Types } from "mongoose";
-
-export interface IAdjacencyItem {
-  node: Types.ObjectId;
-  weight: number;
-}
--- Backend/src/models/types/floorInterface.ts
+++
@@ -1,4 +0,0 @@
-export interface IFloor {
-  id: string;
-  image: string;
-}
